0926a602effa56c745e4a6cd07accdfd2884fc59
Hilmi KALE <hilmikale3139@gmail.com>
2023-08-09 14:15:35 +0300

    self study

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
+
+function App() {
+  const [usersList, setUsersList] = useState([]);
+
+  const addUserHandler = (uName, uLastname, uAge) => {
+    setUsersList((prevUsersList) => {
+      return [
+        ...prevUsersList,
+        {
+          name: uName,
+          lastname: uLastname,
+          age: uAge,
+          id: Math.random().toString(),
+        },
+      ];
+    });
+  };
+
+  return (
+    <div>
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
+    </div>
+  );
+}
+
+export default App;
